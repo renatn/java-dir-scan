6905c20201a1430686a8a43194710f647fbf48f7
renatn <zen@live.ru>
2014-05-06 22:30:20 +0400

    Implemented parse command line

---
+++ src/main/java/CommandType.java
@@ -0,0 +1,8 @@
+/**
+ * Created by renatn on 06.05.2014.
+ */
+public enum CommandType {
+    SCAN,
+    EXIT,
+    HELP
+}
--- src/main/java/Main.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by renatn on 06.05.2014.
- */
-public class Main {
-}
